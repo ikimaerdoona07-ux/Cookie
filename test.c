d9b162f72f87e52b85c3fd8c2b06e82963d79753
ikimaerdoona07-ux <ikimaerdoona07@gmail.com>
2025-09-24 17:17:53 +0200

    my first commit

---
+++ test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main ()
+{
+int my_age;
+my_age = 25;
+
+if (my_age >= 18)
+{
+    printf ("you are an adult");
+
+    
+}
+
+else
+{
+    printf ("you are a child");
+}
+
+
+    return (0);
+}

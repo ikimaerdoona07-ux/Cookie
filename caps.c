fd0297c6e8ae7e45aa9bebe263a8f7ac0c921ea3
ikimaerdoona07-ux <ikimaerdoona07@gmail.com>
2025-12-30 18:07:58 +0100

    Capslock

---
+++ caps.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+ char Letter;
+
+ Letter = 73;
+
+ if (Letter >= 65 && Letter <=90)
+
+{
+    printf ("%c is a capital letter. I will now convert\n", Letter);
+
+    Letter = Letter + 32;
+
+    printf ("%c is now a small letter\n",Letter);
+}
+else if (Letter >= 'a' && Letter <= 'z')
+{
+    printf ( " %c is a small letter. I will now convert\n", Letter);
+
+    Letter = Letter - 32;
+    
+    printf (" %c is now a capital letter\n", Letter);
+}
+ else
+ {
+    printf ( "%c is not an alphabet\n",Letter);
+ }
+
+ return 0;
+
+}
